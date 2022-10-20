a53b64e2abdfca4fda42998276a6bf3c025caf30
quang <61879965+Asusu510@users.noreply.github.com>
2022-10-20 21:10:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Phượng cute';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng não cá vàng';
+const contentCard = 'Chúc bạn Phượng 20/10 tràn ngập niềm vui và những nụ cười. Gửi vũ trụ thông điệp chúc ai đó những ngày tháng sắp tới ngập tràn niềm vui, hạnh phúc và may mắn nhé.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'phuong2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
